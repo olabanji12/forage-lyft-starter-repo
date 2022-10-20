f4883c2036bb67410a4f655134553bbcf62d82cd
olabanji12 <oao190005@utdallas.edu>
2022-10-20 09:38:10 -0500

    modifies Engine class constructor

--- engine/Engine.py
+++ engine/Engine.py
@@ -1,6 +1,6 @@
 class Engine():
-    def __init__(self, last_service_date, warning_light_is_on):
-        self.last_service_date = last_service_date
+    def __init__(self, last_service_mileage, current_mileage, warning_light_is_on):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
         self.warning_light_is_on = warning_light_is_on
-
     
