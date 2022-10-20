e80d3d71b4190a7849d671d4e0cd64af5e315414
olabanji12 <oao190005@utdallas.edu>
2022-10-20 09:30:36 -0500

    defines Engine class constructor

--- engine/Engine.py
+++ engine/Engine.py
@@ -1,2 +1,6 @@
 class Engine():
-    pass
+    def __init__(self, last_service_date, warning_light_is_on):
+        self.last_service_date = last_service_date
+        self.warning_light_is_on = warning_light_is_on
+
+    
