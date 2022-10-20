2fa7a57ad95318048979f1bdc949bc9e7bb24edb
olabanji12 <oao190005@utdallas.edu>
2022-10-20 09:30:56 -0500

    adds Engine object to Car class

--- engine/Car.py
+++ engine/Car.py
@@ -1,7 +1,9 @@
 from abc import ABC
 
+from engine.Engine import Engine
+
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date, warning_light_is_on):
         self.last_service_date = last_service_date
-    
+        self.engine = Engine(last_service_date, warning_light_is_on)
