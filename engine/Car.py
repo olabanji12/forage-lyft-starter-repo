3a8220db99b85f066935078c68b00f549adf908b
olabanji12 <oao190005@utdallas.edu>
2022-10-20 09:49:20 -0500

    adds Engine and Battery object to Car class

--- engine/Car.py
+++ engine/Car.py
@@ -1,9 +1,10 @@
 from abc import ABC
 
-from engine.Engine import Engine
-
+from Engine import Engine
+from Battery import Battery
 
 class Car(ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
+    def __init__(self, last_service_date, warning_light_is_on,last_service_mileage, current_mileage,current_date):
         self.last_service_date = last_service_date
-        self.engine = Engine(last_service_date, warning_light_is_on)
+        self.engine = Engine(last_service_mileage,current_mileage, warning_light_is_on)
+        self.battery = Battery(last_service_date,current_date)
