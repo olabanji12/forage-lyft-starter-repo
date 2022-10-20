37fb8d34948e3b596364a408b6a14ceb41f11349
olabanji12 <oao190005@utdallas.edu>
2022-10-20 10:01:05 -0500

    adds needs service abtract method to Engine and Battery class

--- engine/Battery.py
+++ engine/Battery.py
@@ -1,4 +1,12 @@
+from abc import abstractmethod
+
+
 class Battery():
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
+    
+    
+    @abstractmethod
+    def needs_service(self):
+        pass
--- engine/Engine.py
+++ engine/Engine.py
@@ -1,6 +1,13 @@
+from abc import abstractmethod
+
+
 class Engine():
     def __init__(self, last_service_mileage, current_mileage, warning_light_is_on):
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
         self.warning_light_is_on = warning_light_is_on
     
+
+    @abstractmethod
+    def needs_service(self):
+        pass
