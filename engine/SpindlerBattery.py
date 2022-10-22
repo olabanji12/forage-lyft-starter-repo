ab24594371a4c4a399f9945e46166480d0965ff4
olabanji12 <oao190005@utdallas.edu>
2022-10-22 14:37:01 -0500

    restructure files

--- engine/SpindlerBattery.py
+++
@@ -1,9 +0,0 @@
-from Battery import Battery
-import datetime
-class Nubbin(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
-        self.current_date = current_date
-    
-    def needs_service(self):
-        return self.current_date.year - self.last_service_date.year > 2
