82a2b07f2f0169671827d231a0fb5dd89f5ed080
olabanji12 <oao190005@utdallas.edu>
2022-10-23 20:15:28 -0500

    updates spindler battery servicing requirements

--- Battery/nubbin_battery.py
+++ Battery/nubbin_battery.py
@@ -7,7 +7,7 @@ class Nubbin(Battery):
     
     def needs_service(self):
         date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
-        if date_which_battery_should_be_serviced_by < self.current_date:
+        if self.current_date > date_which_battery_should_be_serviced_by:
             return True
         else:
             return False
--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -6,7 +6,7 @@ from battery.spindler_battery import Spindler
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service_true(self):
-        current_date = date.fromisoformat("2020-05-15")
+        current_date = date.fromisoformat("2021-05-15")
         last_service_date = date.fromisoformat("2018-01-25")
         battery = Spindler(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
